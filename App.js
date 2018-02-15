bbc4f525665915898ad0baa50887c87121dc1063
Megan Wood <meganwood@Megans-MacBook-Pro.local>
2018-02-14 17:40:47 -0800

    fixed it

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 import { TabNavigator, StackNavigator } from 'react-navigation';
-// import { Card, Button } from 'react-native-elements';
-// import Icon from 'react-native-vector-icons/Ionicons';
+import { Card, Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/Ionicons';
 import SettingsScreen from './screens/SettingsScreen';
 import LearningPlan from './screens/LearningPlan';
 import CalendarScreen from './screens/CalendarScreen';
